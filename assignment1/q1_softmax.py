aed3b38d3fbb9d78838805a79e9084586450d4ed
Ilya Rudyak <ilyarudyak@yahoo.com>
2019-01-03 15:27:19 +0300

    done q1 softmax

--- assignment1/q1_softmax.py
+++ assignment1/q1_softmax.py
@@ -31,12 +31,16 @@ def softmax(x):
     if len(x.shape) > 1:
         # Matrix
         ### YOUR CODE HERE
-        raise NotImplementedError
+        x -= np.max(x, axis=1, keepdims=True)
+        x_exp = np.exp(x)
+        x = x_exp / np.sum(x_exp, axis=1, keepdims=True)
         ### END YOUR CODE
     else:
         # Vector
         ### YOUR CODE HERE
-        raise NotImplementedError
+        x -= np.max(x)
+        x_exp = np.exp(x)
+        x = x_exp / np.sum(x_exp)
         ### END YOUR CODE
 
     assert x.shape == orig_shape
@@ -49,39 +53,31 @@ def test_softmax_basic():
     Warning: these are not exhaustive.
     """
     print "Running basic tests..."
-    test1 = softmax(np.array([1,2]))
+    test1 = softmax(np.array([1, 2]))
     print test1
-    ans1 = np.array([0.26894142,  0.73105858])
+    ans1 = np.array([0.26894142, 0.73105858])
+    print ans1
+    print
     assert np.allclose(test1, ans1, rtol=1e-05, atol=1e-06)
 
-    test2 = softmax(np.array([[1001,1002],[3,4]]))
+    test2 = softmax(np.array([[1001, 1002], [3, 4]]))
     print test2
     ans2 = np.array([
         [0.26894142, 0.73105858],
         [0.26894142, 0.73105858]])
+    print ans2
+    print
     assert np.allclose(test2, ans2, rtol=1e-05, atol=1e-06)
 
-    test3 = softmax(np.array([[-1001,-1002]]))
+    test3 = softmax(np.array([[-1001, -1002]]))
     print test3
     ans3 = np.array([0.73105858, 0.26894142])
+    print ans3
     assert np.allclose(test3, ans3, rtol=1e-05, atol=1e-06)
 
     print "You should be able to verify these results by hand!\n"
 
 
-def test_softmax():
-    """
-    Use this space to test your softmax implementation by running:
-        python q1_softmax.py
-    This function will not be called by the autograder, nor will
-    your tests be graded.
-    """
-    print "Running your tests..."
-    ### YOUR CODE HERE
-    raise NotImplementedError
-    ### END YOUR CODE
-
-
 if __name__ == "__main__":
     test_softmax_basic()
-    test_softmax()
+    # test_softmax()
