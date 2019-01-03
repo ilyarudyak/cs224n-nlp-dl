ad7917a10859630bba1cd1edbe09a1332f8c8df1
Ilya Rudyak <ilyarudyak@yahoo.com>
2019-01-03 19:16:55 +0300

    done sigmoid

--- assignment1/q2_sigmoid.py
+++ assignment1/q2_sigmoid.py
@@ -15,7 +15,7 @@ def sigmoid(x):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    s = 1 / (1 + np.exp(-x))
     ### END YOUR CODE
 
     return s
@@ -35,7 +35,7 @@ def sigmoid_grad(s):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    ds = s * (1 - s)
     ### END YOUR CODE
 
     return ds
@@ -49,33 +49,21 @@ def test_sigmoid_basic():
     print "Running basic tests..."
     x = np.array([[1, 2], [-1, -2]])
     f = sigmoid(x)
-    g = sigmoid_grad(f)
     print f
     f_ans = np.array([
         [0.73105858, 0.88079708],
         [0.26894142, 0.11920292]])
     assert np.allclose(f, f_ans, rtol=1e-05, atol=1e-06)
+
+    g = sigmoid_grad(f)
     print g
     g_ans = np.array([
         [0.19661193, 0.10499359],
         [0.19661193, 0.10499359]])
     assert np.allclose(g, g_ans, rtol=1e-05, atol=1e-06)
+
     print "You should verify these results by hand!\n"
 
 
-def test_sigmoid():
-    """
-    Use this space to test your sigmoid implementation by running:
-        python q2_sigmoid.py
-    This function will not be called by the autograder, nor will
-    your tests be graded.
-    """
-    print "Running your tests..."
-    ### YOUR CODE HERE
-    raise NotImplementedError
-    ### END YOUR CODE
-
-
 if __name__ == "__main__":
-    test_sigmoid_basic();
-    test_sigmoid()
+    test_sigmoid_basic()
