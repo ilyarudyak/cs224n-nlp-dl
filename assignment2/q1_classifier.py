729f465a82ceedd98f5f6960a01807be696e0298
Ilya Rudyak <ilyarudyak@yahoo.com>
2019-01-16 18:52:43 +0300

    done feed dict

--- assignment2/q1_classifier.py
+++ assignment2/q1_classifier.py
@@ -75,6 +75,8 @@ class SoftmaxModel(Model):
             feed_dict: The feed dictionary mapping from placeholders to values.
         """
         ### YOUR CODE HERE
+        feed_dict = {self.input_placeholder: inputs_batch,
+                     self.labels_placeholder: labels_batch}
         ### END YOUR CODE
         return feed_dict
 
