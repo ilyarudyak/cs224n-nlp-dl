9aaf7d97d761f91dc79b34e7e57441ae226135c8
Ilya Rudyak <ilyarudyak@yahoo.com>
2019-05-22 00:28:53 +0300

    done unigram model

--- cs124/hw2_spelling/src/LaplaceUnigramLanguageModel.py
+++ cs124/hw2_spelling/src/LaplaceUnigramLanguageModel.py
@@ -1,20 +1,33 @@
+from collections import defaultdict
+from math import log
+
+
 class LaplaceUnigramLanguageModel:
 
-  def __init__(self, corpus):
-    """Initialize your data structures in the constructor."""
-    # TODO your code here
-    self.train(corpus)
+    def __init__(self, corpus):
+        """Initialize your data structures in the constructor."""
+        self.unigramCounts = defaultdict(int)
+        self.totalWords = 0
+        self.train(corpus)
+        self.uniqueWords = len(self.unigramCounts)
 
-  def train(self, corpus):
-    """ Takes a corpus and trains your language model. 
+    def train(self, corpus):
+        """ Takes a corpus and trains your language model.
         Compute any counts or other corpus statistics in this function.
-    """  
-    # TODO your code here
-    pass
+        """
+        for sentence in corpus.corpus:
+            for datum in sentence.data:
+                token = datum.word
+                self.unigramCounts[token] = self.unigramCounts[token] + 1
+                self.totalWords += 1
 
-  def score(self, sentence):
-    """ Takes a list of strings as argument and returns the log-probability of the 
+    def score(self, sentence):
+        """ Takes a list of strings as argument and returns the log-probability of the
         sentence using your language model. Use whatever data you computed in train() here.
-    """
-    # TODO your code here
-    return 0.0
+        """
+        score = 0.0
+        for token in sentence:
+            count = self.unigramCounts[token]
+            score += log(count + 1)
+            score -= log(self.totalWords + self.uniqueWords)
+        return score
